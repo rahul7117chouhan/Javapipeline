adf6bfba03df92ab4ec06789a230fcf6db6b3008
Rahul Chouhan <rahul7117chouhan@gmail.com>
2025-08-07 00:08:43 +0530

    First Commit

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 7;
+
+        int sum = a + b;
+        int difference = a - b;
+
+        System.out.println("Addition of " + a + " and " + b + " is: " + sum);
+        System.out.println("Subtraction of " + b + " from " + a + " is: " + difference);
+    }
+}
